57ff4df559d3ac0d9a34e8c784cf158317d94ca3
Nate <nathanestes@gmail.com>
2018-10-10 21:43:01 -0600

    playing with multiple items

--- basic.js
+++ basic.js
@@ -14,8 +14,23 @@ const move = () => {
   }
 }
 
-
+const slide = () => {
+  console.log('moving2')
+  let elem = document.getElementById('animation2')
+  let pos = 0
+  const id = setInterval(frame, 10)
+  function frame() {
+    if (pos === 350) {
+      clearInterval(id)
+    } else {
+      pos++
+      elem.style.top = `${pos}px`;
+      elem.style.left = `${pos}px`;
+    }
+  }
+}
 
 
 const btn = document.getElementById('start')
 btn.addEventListener('click', move)
+btn.addEventListener('click', slide)
